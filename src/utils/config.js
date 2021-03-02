cc23d39eb5e4d04cbc9099ff6bad064c5c25ff51
hcc96923 <54052873+hcc96923@users.noreply.github.com>
2021-03-02 16:23:55 +0800

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,9 @@
 // 测试生产环境
 export let SERVER_ADDRESS;
 if (process.env.NODE_ENV === 'development') { 
-    SERVER_ADDRESS = 'http://localhost:5000';
+    SERVER_ADDRESS = 'http://localhost:3001';
 } else { 
-    SERVER_ADDRESS = 'http://116.62.139.167:5000';
+    SERVER_ADDRESS = 'http://116.62.139.167:3001';
 };
 
 

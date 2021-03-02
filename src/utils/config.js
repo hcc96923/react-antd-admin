c6c71667055395f982061091648dba722ed33098
hcc96923 <54052873+hcc96923@users.noreply.github.com>
2021-03-02 16:23:29 +0800

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,11 @@
-// 测试环境
-export const SERVER_ADDRESS = 'http://localhost:5000';
-// 生产环境
-// export const SERVER_ADDRESS = 'http://116.62.139.167:5000';
+// 测试生产环境
+export let SERVER_ADDRESS;
+if (process.env.NODE_ENV === 'development') { 
+    SERVER_ADDRESS = 'http://localhost:5000';
+} else { 
+    SERVER_ADDRESS = 'http://116.62.139.167:5000';
+};
+
+
 // 邮箱验证码密钥
 export const EMAIL_KEY = 'emailSecretKey';
